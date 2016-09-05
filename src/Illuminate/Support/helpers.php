0b2c66646dda28677b7028d47868256c86593dda
Johnathon Koster <john@stillat.com>
2016-09-05 01:53:38 -0500

    Updated to v4.0.0 changes.

--- src/Illuminate/Support/helpers.php
+++ src/Illuminate/Support/helpers.php
@@ -46,3 +46,16 @@ if (! function_exists('array_flatten')) {
 		return $return;
 	}
 }
+
+if (! function_exists('value')) {
+	/**
+	 * Return the default value of the given value.
+	 *
+	 * @param  mixed  $value
+	 * @return mixed
+	 */
+	function value($value)
+	{
+		return $value instanceof Closure ? $value() : $value;
+	}
+}
